7e71a840c1e841c835aa2ea65b81e64de36a4b69
Ingo Schlapschy <ingo.schlapschy@gmail.com>
2026-02-08 17:27:19 +0100

    install cosmic DE

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,6 +12,7 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf5 install -y tmux 
 dnf5 install -y cmatrix 
+dnf5 install -y @cosmic-desktop-environment 
 
 # Use a COPR Example:
 #
