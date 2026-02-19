3f21aadd51271f5128daf254267a4cf71d6004d5
Ingo Schlapschy <ingo.schlapschy@gmail.com>
2026-02-19 17:25:36 +0100

    include niri & quickshell; remove cosmic-de (complete suite)

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,9 +10,12 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
-dnf5 install -y cmatrix 
-dnf5 install -y @cosmic-desktop-environment 
+dnf5 install -y niri
+
+# COPR install
+dnf5 -y copr enable errornointernet/quickshell
+dnf5 -y install quickshell
+dnf5 -y copr disable errornointernet/quickshell
 
 # Use a COPR Example:
 #
